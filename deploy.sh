ff17f1f1b3cb48bf608341e77724764251ac20fd
mitzepx01 <mitzepx01@gmail.com>
2024-02-29 19:44:07 -0600

    Update deploy.sh
    WAZUH_VERSION

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ echo -e "Hi!..."
 
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf && sysctl -p
 docker compose -f "${D1R}/compose-certs-generator.yml" run --rm generator
-WI_HASH="$(docker run --rm -ti wazuh/wazuh-indexer:4.3.6 bash /usr/share/wazuh-indexer/plugins/opensearch-security/tools/hash.sh -p $WI_PASS)"
+WI_HASH="$(docker run --rm -ti wazuh/wazuh-indexer:$WAZUH_VERSION bash /usr/share/wazuh-indexer/plugins/opensearch-security/tools/hash.sh -p $WI_PASS)"
 WI_HASH=$(echo -n $WI_HASH | tr -d '\r')
 sed -i "s|set_hash|${WI_HASH}|g" "${D1R}/config/wazuh-indexer/internal_users.yml"
 
